781f7e72a0d066d32c3534ec310e15d00f1b37a8
Marcus <mdk0208@icloud.com>
2026-04-22 09:18:18 +0200

    Vi har fået sat api op

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./app";
+import Home from "./components/home";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   </BrowserRouter>,
 );
